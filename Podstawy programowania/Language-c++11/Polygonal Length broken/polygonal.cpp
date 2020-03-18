3dda20036ffd386dbf48dcebbe73eb56fe432d8f
Ruslan <45178528+RuslanLytvynov@users.noreply.github.com>
2020-03-18 23:09:16 +0100

    Add files via upload

--- Podstawy programowania/Language-c++11/Polygonal Length broken/polygonal.cpp
+++ Podstawy programowania/Language-c++11/Polygonal Length broken/polygonal.cpp
@@ -3,18 +3,16 @@
 
 using namespace std;
 
-double polygonal()
-{
-    double x1,x2,y1,y2,sum=0;
-    for(int index=0; cin>>x1>>y1>>x2>>y2; index++)
-    {
-        sum = sum + sqrt(pow(x2-x1,2)+pow(y2-y1,2));
-    }
-    cout << sum;
-}
 
 int main()
 {
-    polygonal();
-    return 0;
+  double x1, x2, y1, y2;
+     double sum = 0;
+
+     cin >> x1 >> y1;
+     while (cin >> x2 >> y2) {
+           sum += sqrt(pow(x2-x1,2)+pow(y2-y1,2));
+           x1 = x2; y1 = y2;
+     }
+     cout << sum;
 }
