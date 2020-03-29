2c4f25cc5f253942661083320e47793607d97a14
Ruslan <45178528+RuslanLytvynov@users.noreply.github.com>
2020-03-29 18:29:13 +0200

    Done

--- Podstawy programowania/Language-c++11/Function Prime/prime.cpp
+++ Podstawy programowania/Language-c++11/Function Prime/prime.cpp
@@ -1,31 +1,51 @@
-#include<iostream>
-#include<cstdlib>
+#include <iostream>
 using namespace std;
 
-bool prime(int n)
-{
-	if(n<2)
-		return false; //gdy liczba jest mniejsza niż 2 to nie jest pierwszą
 
-	for(int i=2;i*i<=n;i++)
-		if(n%i==0)
-			return false; //gdy znajdziemy dzielnik, to dana liczba nie jest pierwsza
-	return true;
+bool prime(int num){
+    if (num < 2) {
+        return 0;
+    }
+    for (int i = 2; i < num; i++) {
+        if (num % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
 }
 
-int main()
-{
-	int n;
 
-	cout<<"Podaj liczbę: ";
-	cin>>n;
-
-	if(prime(n)) //lub czy_pierwsza(n)==1
-		cout<<"true"<<endl;
-	else
-		cout<<"false"<<endl;
-	return 0;
+int main() {
+    cout << boolalpha << prime(97) << endl;
 }
+
+// #include<iostream>
+// #include<cstdlib>
+// using namespace std;
+//
+// bool prime(int n)
+// {
+// 	if(n<2)
+// 		return false; //gdy liczba jest mniejsza niż 2 to nie jest pierwszą
+//
+// 	for(int i=2;i*i<=n;i++)
+// 		if(n%i==0)
+// 			return false; //gdy znajdziemy dzielnik, to dana liczba nie jest pierwsza
+// 	return true;
+// }
+//
+// int main()
+// {
+// 	int n;
+//
+// 	cin>>n;
+//
+// 	if(prime(n)) //lub prime(n)==1
+// 		cout<<"true"<<endl;
+// 	else
+// 		cout<<"false"<<endl;
+// 	return 0;
+// }
 
 
 // #include<iostream>
