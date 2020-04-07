3801e99800613cca54749b11d99e91e9dd6f560c
Ruslan <45178528+RuslanLytvynov@users.noreply.github.com>
2020-04-07 07:50:48 +0200

    Done
    + 2 task

---
+++ Podstawy programowania/Language-c++11/Argumenty/Nominals Odliczanie kwoty w nominałach/Nominals.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+
+
+int main(int argc, char *argv[]) {
+    int n = std::atof(argv[1]);
+    std::vector<int> nom = {1,2,5,10,20,50,100,200};
+    int x = nom.size() - 1;
+
+    while (n != 0) {
+        if (nom[x] <= n) {
+            std::cout << nom[x] << " ";
+            n -= nom[x];
+        }
+        else {
+            x -= 1;
+        }
+    }
+    std::cout << std::endl;
+}
