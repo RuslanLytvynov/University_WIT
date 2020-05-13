75b6619df37aed8e575ace510d026ddd0fd7640c
Ruslan <45178528+RuslanLytvynov@users.noreply.github.com>
2020-05-13 11:15:40 +0200

    work at lesson

--- Podstawy programowania/Language-c++11/C++_Lesson/Lesson 11 13.05.2020/remove.cpp
+++ Podstawy programowania/Language-c++11/C++_Lesson/Lesson 11 13.05.2020/remove.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include "vector"
 
-oid selection_sort(std::vector<int>::iterator b,
-                    std::vector<int>::iterator e, int n){
-
-}
+// std::vector<int>::iterator find(std::vector<int>::iterator b,
+//                     std::vector<int>::iterator e, int n)
+std::vector<int>::iterator find(std::vector<int>::iterator b,
+                              std::vector<int>::iterator e, int n){
+for (; b < e; ++b) {
+  if (*b == n) {
+  return b;}
+}}
 
 int main(){
   std::vector<int> v {-7, 5, 2, 2, 11, 2, 3};
-  auto r = find(v.begin(), v.end(), 11);
+  auto r = find(v.begin(), v.end(), 17);
   std::cout << r - v.begin() << std::endl;
 }
