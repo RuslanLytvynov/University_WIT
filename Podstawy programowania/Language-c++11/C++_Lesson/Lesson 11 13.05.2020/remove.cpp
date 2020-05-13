f717e477785d8a87efb7582f472a27b5f48eab0d
Ruslan <45178528+RuslanLytvynov@users.noreply.github.com>
2020-05-13 11:59:25 +0200

    work at lesson 2.0

--- Podstawy programowania/Language-c++11/C++_Lesson/Lesson 11 13.05.2020/remove.cpp
+++ Podstawy programowania/Language-c++11/C++_Lesson/Lesson 11 13.05.2020/remove.cpp
@@ -3,16 +3,29 @@
 
 // std::vector<int>::iterator find(std::vector<int>::iterator b,
 //                     std::vector<int>::iterator e, int n)
-std::vector<int>::iterator find(std::vector<int>::iterator b,
+std::vector<int>::iterator find_not(std::vector<int>::iterator b,
                               std::vector<int>::iterator e, int n){
 for (; b < e; ++b) {
-  if (*b == n) {
+  if (*b != n) {
   return b;}}
   return e;
 }
 
+void remove(std::vector<int>::iterator b,
+            std::vector<int>::iterator e, int n){
+
+  auto i = find_not(b, e, n);
+while (true){
+  i = find_not(i, e, n);
+  if (i == e){
+    return;
+  }
+  *b++ = *i++;}}
+
 int main(){
   std::vector<int> v {-7, 5, 2, 2, 11, 2, 3};
-  auto r = find(v.begin(), v.end(), 17);
-  std::cout << r - v.begin() << std::endl;
-}
+  auto r = remove(v.begin(), v.end(), 2);
+  v.erase(r, b.end());
+  for (auto i = v.begin(); i < v.end(); ++i){
+      std::cout << *i << " ";}
+    std::cout << std::endl;}
