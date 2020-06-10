289765efd6be569e8afa71c62085eb1320122ffc
Ruslan <45178528+RuslanLytvynov@users.noreply.github.com>
2020-06-10 08:59:46 +0200

    new code

--- Podstawy programowania/Language-c++11/HomeWork/Reveiw/Algorytmy/Permutation/Permutation.cpp
+++ Podstawy programowania/Language-c++11/HomeWork/Reveiw/Algorytmy/Permutation/Permutation.cpp
@@ -3,17 +3,43 @@
 #include<algorithm>
 #include <cstdlib>
 #include <ctime>
+using namespace std;
 
-
-std::vector<int> permutation(
-  std::vector<int> number)
+int getNum(vector<int>& v)
 {
-  std::vector<int> v2 = number;
-  std::iota(v2.begin(), v2.end(), number);
-  std::shuffle(v2.begin(), v2.end());
-
-return v2;
+    // Size of the vector
+    int n = v.size();
+    // Generate a random number
+    srand(time(NULL));
+    // Make sure the number is within
+    // the index range
+    int index = rand() % n;
+    // Get random number from the vector
+    int num = v[index];
+    // Remove the number from the vector
+    swap(v[index], v[n - 1]);
+    v.pop_back();
+    // Return the removed number
+    return num;
 }
+
+std::vector<int> permutation(int n)
+{
+    vector<int> v(n);
+
+    // Fill the vector with the values
+    // 1, 2, 3, ..., n
+    for (int i = 0; i < n - 1; i++)
+        v[i] = i + 1;
+
+    // While vector has elements
+    // get a random number from the vector and print it
+    while (v.size()) {
+        cout << getNum(v) << " ";
+    }
+}
+
+
 
 int main() {
   std::srand(std::time(nullptr));
